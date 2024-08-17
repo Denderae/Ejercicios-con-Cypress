dedc66eebf14177a88f570f3d57b65b576e83a68
Camilo Campos Sterling <105098838+Denderae@users.noreply.github.com>
2024-08-17 00:43:17 -0500

    Update TestKatalon.cy.js

--- TestKatalon.cy.js
+++ TestKatalon.cy.js
@@ -1,6 +1,8 @@
+
 describe('Pruebas E2E', () => {
     beforeEach(() => {
         cy.visit("https://katalon-demo-cura.herokuapp.com/")
+
     })
    
 
@@ -17,8 +19,20 @@ describe('Pruebas E2E', () => {
         cy.get('tbody > :nth-child(3) > :nth-child(5)').click()    
         cy.get('#txt_comment').type("Test numero 1")
         cy.contains("Book Appointment").click()
-        cy.get('.col-xs-8').should('contain', 'Tokyo CURA Healthcare Center','Yes','Medicare','15/08/2024','Test numero 1')//Se valida contenido del formulario llenado
-        cy.get('.text-center > .btn').click()
+        // Selecciona el contenedor con la clase 'col-xs-8'
+        cy.get('.col-xs-8')
+            // Verifica que contenga el texto 'Tokyo CURA Healthcare Center'
+            .should('contain', 'Tokyo CURA Healthcare Center')
+            // Verifica que contenga el texto 'Yes'
+            .and('contain', 'Yes')
+            // Verifica que contenga el texto 'Medicare'
+            .and('contain', 'Medicare')
+            // Verifica que contenga la fecha '15/08/2024'
+            .and('contain', '15/08/2024')
+            // Verifica que contenga el texto 'Test numero 1'
+            .and('contain', 'Test numero 1')
+
+        cy.contains("Go to Homepage").click()
     })
 
     it("Prueba E2E Happy path formulario segundas opciones del formulario", () => {
@@ -31,8 +45,18 @@ describe('Pruebas E2E', () => {
         cy.get('#radio_program_medicaid').check()
         cy.get('#txt_visit_date').click()
         cy.get('tbody > :nth-child(5) > :nth-child(7)').click()
-        cy.get('tbody > :nth-child(5) > :nth-child(7)').should('not.be.visible')
         cy.contains("Book Appointment").click()
+        cy.get('.col-xs-8')
+            // Verifica que contenga el texto 'Hongkong CURA Healthcare Center'
+            .should('contain', 'Hongkong CURA Healthcare Center')
+            // Verifica que contenga el texto 'Yes'
+            .and('contain', 'Yes')
+            // Verifica que contenga el texto 'Medicaid'
+            .and('contain', 'Medicaid')
+            // Verifica que contenga la fecha '31/08/2024'
+            .and('contain', '31/08/2024')
+
+        cy.contains("Go to Homepage").click()
     })
 
     it("Prueba E2E Happy path formulario terceras opciones del formulario", () => {
@@ -45,9 +69,21 @@ describe('Pruebas E2E', () => {
         cy.get('#radio_program_none').check()
         cy.get('#txt_visit_date').click()
         cy.get('tbody > :nth-child(4) > :nth-child(4)').click()
-        cy.get('tbody > :nth-child(4) > :nth-child(4)').should('not.be.visible')
+        cy.get('tbody > :nth-child(3) > :nth-child(5)').should('not.be.visible') //Deberia cerrarse el formulario calendario despues del clic
         cy.get('#txt_comment').type("Test numero 2")
         cy.contains("Book Appointment").click()
+        cy.get('.col-xs-8')
+        // Verifica que contenga el texto 'Seoul CURA Healthcare Center     '
+        .should('contain', 'Seoul CURA Healthcare Center')
+        // Verifica que contenga el texto 'Yes'
+        .and('contain', 'Yes')
+        // Verifica que contenga el texto 'None'
+        .and('contain', 'None')
+        // Verifica que contenga la fecha '31/08/2024'
+        .and('contain', '21/08/2024')
+        // Verifica que contenga el texto 'Test numero 2'
+        .and('contain', 'Test numero 2')
+    cy.contains("Go to Homepage").click()
 
     })
 
@@ -56,17 +92,13 @@ describe('Pruebas E2E', () => {
         cy.get('#txt-username').type("John Doe8")
         cy.get('#txt-password').type("ThisIsNotAPassword8")
         cy.get('#btn-login').click()
-        cy.get('#combo_facility').select("Seoul CURA Healthcare Center")
-        cy.get('#chk_hospotal_readmission').check() 
-        cy.get('#radio_program_none').check()
-        cy.get('#txt_visit_date').click()
-        cy.get('tbody > :nth-child(4) > :nth-child(4)').click()
-        cy.get('tbody > :nth-child(4) > :nth-child(4)').should('not.be.visible')
-        cy.contains("Book Appointment").click()
+        //Se espera mensaje de error login fallido
+        cy.contains("Login failed! Please ensure the username and password are valid").should('be.visible')
+        
 
     })
 
-    it("Prueba negativa no se llena campo requerido fecha ", () => {
+    it.only("Prueba negativa no se llena campo requerido fecha ", () => {
         cy.get('#btn-make-appointment').click()
         cy.get('#txt-username').type("John Doe")
         cy.get('#txt-password').type("ThisIsNotAPassword")
@@ -76,6 +108,7 @@ describe('Pruebas E2E', () => {
         cy.get('#radio_program_none').check()
         cy.get('#txt_comment').type("Test numero 3")
         cy.contains("Book Appointment").click()
+        cy.contains("Please fill out this field.").invoke('show').should('be.visible')
     })
 
 })
